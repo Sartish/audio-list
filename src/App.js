30d9d2be2faee207a7a34299b62db211132ecbc2
Sara Carlstein <carlstein.sara@gmail.com>
2021-03-29 10:02:00 +0200

    a start

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import AlbumList from './pages/AlbumList'
 
 export const App = () => {
   return (
     <div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact>
+            <AlbumList />
+          </Route>
+        </Switch>
+      </BrowserRouter>
       Find me in src/app.js!
     </div>
   )
